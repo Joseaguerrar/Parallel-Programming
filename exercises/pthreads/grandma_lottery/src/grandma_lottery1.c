b5c165024adc4718c299e3fe8d9955548710d817
joseaguerrar <joseaguerrar2017@gmail.com>
2024-08-26 23:16:19 -0600

    creación de las semillas e hilos, ya me voy a dormir...

--- exercises/pthreads/grandma_lottery/src/grandma_lottery1.c
+++ exercises/pthreads/grandma_lottery/src/grandma_lottery1.c
@@ -5,7 +5,12 @@
 void* buy_lottery(void*);
 
 int main(){
+    pthread_t nieto1,nieto2;
+    int semilla1=1, semilla2=2;
 
+    //creación de los hilos representando los nietos
+    pthread_create(&nieto1,NULL,buy_lottery,&semilla1);
+    pthread_create(&nieto2,NULL,buy_lottery,&semilla2);
 
 
 
@@ -16,7 +21,7 @@ int main(){
 
 
 void* buy_lottery(void* arg){
-    int* num_lottery= malloc(sizeof(int));
+    __uint16_t* num_lottery= malloc(sizeof(__uint16_t));
     //reservamos espacio en memoria para el entero
     if(num_lottery==NULL){
         //si es nulo, hubo error
@@ -24,8 +29,12 @@ void* buy_lottery(void* arg){
         exit(EXIT_FAILURE);
         //devolvemos salida erronea
     }
-    *num_lottery=rand_r((unsigned int*)arg)%100;
+    *num_lottery=rand_r(( __uint16_t*)arg)%100;
     //le decimos que el valor que va a guardar es un random positivo de 0 a 99
+
+    /*rand_r es una versión de rand que es reentrante, lo cual es importante en un entorno multihilo. 
+    Estás pasando un puntero al argumento arg, lo que permite que rand_r genere un número pseudoaleatorio
+    basado en la semilla proporcionada. Definición obtenida de chatgpt para ver al diferencia entre rand y rand_r*/
     return (void*)num_lottery;
     //lo retornamos tratandolo como void*
 }
--- exercises/pthreads/grandma_lottery/src/grandma_lottery2.c
+++ exercises/pthreads/grandma_lottery/src/grandma_lottery2.c
@@ -1,3 +1,13 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<pthread.h>
+void* buy_lottery(void*);
+
+int main(){
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
