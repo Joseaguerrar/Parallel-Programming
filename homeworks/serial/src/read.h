2787ed3b7331b8b5b3913d4cb5d305c88125625a
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-03 11:09:58 -0600

    Modificación de formato con doxygen ya que decía que habían duplicados

--- homeworks/serial/src/read.h
+++ homeworks/serial/src/read.h
@@ -10,13 +10,6 @@
 /**
  * @brief Lee los datos desde un archivo y almacena la matriz de temperaturas y parámetros de simulación.
  * 
- * @param file Nombre del archivo de entrada.
- * @param filas Puntero a un entero donde se almacenará el número de filas de la matriz.
- * @param columnas Puntero a un entero donde se almacenará el número de columnas de la matriz.
- * @param epsilon Puntero a un `double` donde se almacenará el valor de epsilon (criterio de convergencia).
- * @param dtime Puntero a un `double` donde se almacenará el diferencial de tiempo (Δt).
- * @param alpha Puntero a un `double` donde se almacenará la difusividad térmica del material.
- * @param height Puntero a un `double` donde se almacenará el tamaño de las celdas (h).
  * @return double** Un puntero doble que apunta a la matriz de temperaturas.
  */
 double ** read_file(const char *file, int *filas, int *columnas,
@@ -25,10 +18,6 @@ double *epsilon, double *dtime, double *alpha, double *height);
 /**
  * @brief Imprime el resultado de la simulación en un archivo.
  * 
- * @param file Nombre del archivo de salida.
- * @param matriz Puntero doble que apunta a la matriz de temperaturas.
- * @param filas Número de filas de la matriz.
- * @param columnas Número de columnas de la matriz.
  */
 void print_result(const char *file, double **matriz, int filas, int columnas);
 
