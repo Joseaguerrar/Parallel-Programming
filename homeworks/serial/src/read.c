63272ae3f9f6c6f76dd00357a7591b599bcc43fa
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-02 20:02:45 -0600

    Seguimiento de la función de lectura

--- homeworks/serial/src/read.c
+++ homeworks/serial/src/read.c
@@ -6,9 +6,17 @@
 //implementación de función para leer archivo
 double ** read_file(const char *file, int *filas, int *columnas, double *epsilon, 
 double *dtime, double *alpha, double *height) {
+    //Abrimos el archivo, si es nulo, tira error
     FILE *fp = fopen(file, "rb");
     if (!fp) {
         perror("Error al abrir el archivo");
         exit(EXIT_FAILURE);
     }
+
+    //Para leer archivos binarios
+    fread(filas, sizeof(int), 1, fp);
+    fread(columnas, sizeof(int), 1, fp);
+    
+    //creamos la matriz con los valores obtenidos
+    double **matriz = asign_matrix(*filas, *columnas);
 }
