b8b69a6dec094c7df149a1e9e7d1a2bc41fad08d
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-02 18:30:44 -0600

    Implementación para liberar la matriz

--- homeworks/serial/src/matrix_operations.c
+++ homeworks/serial/src/matrix_operations.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-//implementación
+//implementación para la actualización de temperatura de la matriz
 double update_temp(double **matriz, int filas, int columnas, double dtime,
 double alpha, double height, double epsilon) {
     //variable para guardar el cambio máximo de temperatura de la matriz
@@ -11,7 +11,8 @@ double alpha, double height, double epsilon) {
     // Crear una matriz temporal para almacenar los nuevos valores
     double **temp_matriz = asign_matrix(filas, columnas);
 }
-//implementación
+
+//implementación para la asignación de la matriz
 double** asign_matrix(int filas, int columnas) {
     //Reservamos espacio para las filas(apuntan a arrays de doubles)
     double **matriz = (double **)malloc(filas * sizeof(double *));
@@ -20,6 +21,14 @@ double** asign_matrix(int filas, int columnas) {
         matriz[i] = (double *)malloc(columnas * sizeof(double));
     }
     return matriz;
+}
+
+//implementación para liberar la matriz de memoria
+void free_matrix(double **matriz, int filas) {
+    for (int i = 0; i < filas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
 }
 
 
