da1894ba2335bbae0fa7826b73715ae90c4fa1da
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-03 11:09:58 -0600

    Modificación de formato con doxygen ya que decía que habían duplicados

--- homeworks/serial/src/matrix_operations.h
+++ homeworks/serial/src/matrix_operations.h
@@ -10,13 +10,6 @@
 /**
  * @brief Actualiza las temperaturas en la matriz de acuerdo con la fórmula de transferencia de calor.
  * 
- * @param matriz Puntero doble que apunta a la matriz de temperaturas.
- * @param filas Número de filas en la matriz.
- * @param columnas Número de columnas en la matriz.
- * @param dtime Diferencial de tiempo (Δt).
- * @param alpha Difusividad térmica del material.
- * @param height Tamaño de las celdas (h).
- * @param epsilon Valor de epsilon que representa el criterio de convergencia.
  * @return double El cambio máximo de temperatura en la matriz durante esta actualización.
  */
 double update_temp(double **matriz, int filas, int columnas, double dtime,
@@ -25,17 +18,12 @@ double alpha, double height);
 /**
  * @brief Crea una matriz bidimensional con el número especificado de filas y columnas.
  * 
- * @param filas Número de filas en la matriz.
- * @param columnas Número de columnas en la matriz.
  * @return double** Un puntero doble que apunta a la matriz creada.
  */
 double** asign_matrix(int filas, int columnas);
 
 /**
  * @brief Libera el espacio de memoria ocupado por una matriz bidimensional.
- * 
- * @param matriz Puntero doble que apunta a la matriz de temperaturas.
- * @param filas Número de filas en la matriz.
  */
 void free_matrix(double **matriz, int filas);
 
