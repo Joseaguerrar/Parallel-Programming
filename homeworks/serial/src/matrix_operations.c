7fa1a1e53f5eb95b83bf7271e37881799bc3c8b5
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-02 18:15:42 -0600

    Primera parte de la implementación de la función para actualizar temperatura de matriz

---
+++ homeworks/serial/src/matrix_operations.c
@@ -0,0 +1,16 @@
+#include <matrix_operations.h>
+#include <stdlib.h>
+#include <math.h>
+
+//implementación
+double update_temp(double **matriz, int filas, int columnas, double dtime,
+double alpha, double height, double epsilon) {
+    //variable para guardar el cambio máximo de temperatura de la matriz
+    double max_change = 0.0;
+
+    // Crear una matriz temporal para almacenar los nuevos valores
+    double **temp_matriz = asign_matrix(filas, columnas);
+}
+
+
+
