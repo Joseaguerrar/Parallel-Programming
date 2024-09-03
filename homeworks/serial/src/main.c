f250f1f20b255a207a14346b0e31af20269412fc
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-03 10:43:06 -0600

    Modificación del código a convención de linter

--- homeworks/serial/src/main.c
+++ homeworks/serial/src/main.c
@@ -1,3 +1,4 @@
+//  Copyright [2024] <jose.guerrarodriguez@ucr.ac.cr>
 #include <read.h>
 #include <matrix_operations.h>
 #include <stdio.h>
@@ -5,28 +6,41 @@
 
 
 
-int main(int argc, char *argv[]){
-    if (argc<2)
-    {
+int main(int argc, char *argv[]) {
+    //  Verificar que se haya pasado el nombre del archivo de entrada
+    if (argc < 2) {
         fprintf(stderr, "Uso: %s <archivo de entrada>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    const char *input_file= argv[1];
+    const char *input_file = argv[1];
 
     int filas, columnas;
     double epsilon, dtime, alpha, height;
 
-    double **matriz= read_file(input_file, &filas, &columnas, &epsilon, &dtime, &alpha, &height);
-    
-    int k=0;
-    double max_change;
+    //  Leer la matriz y parámetros de simulación desde el archivo binario
+    double **matriz = read_file(input_file, &filas, &columnas,
+    &epsilon, &dtime, &alpha, &height);
 
-    while (max_change> epsilon)
-    {
-        max_change=update_temp(matriz, filas, columnas, dtime,alpha,height,epsilon);
+    int k = 0;
+    double max_change = 0.0;
+    //  Realizar iteraciones hasta que el cambio máximo sea menor que epsilon
+    while (max_change> epsilon) {
+        max_change = update_temp(matriz, filas, columnas, dtime, alpha, height);
         k++;
     }
-    
+    //  Mostrar el número de iteraciones hasta alcanzar el equilibrio
     printf("Equilibrio alcanzado en %d iteraciones.\n", k);
-}
+
+    /* Preparar el nombre del archivo de salida 
+    basado en el número de iteraciones*/
+    char output_file[256];
+    snprintf(output_file, sizeof(output_file), "output_plate_%d.bin", k);
+
+    // Guardar la matriz de temperaturas final en un archivo binario
+    print_result(output_file, matriz, filas, columnas);
+
+    // Liberar la memoria asignada para la matriz
+    free_matrix(matriz, filas);
+    return EXIT_SUCCESS;
+}  // endprocedure
--- homeworks/serial/src/matrix_operations.c
+++ homeworks/serial/src/matrix_operations.c
@@ -1,57 +1,59 @@
+//  Copyright [2024] <jose.guerrarodriguez@ucr.ac.cr>
 #include <matrix_operations.h>
 #include <stdlib.h>
 #include <math.h>
 
-//implementación para la actualización de temperatura de la matriz
+//  implementación para la actualización de temperatura de la matriz
 double update_temp(double **matriz, int filas, int columnas, double dtime,
-double alpha, double height, double epsilon) {
-    //variable para guardar el cambio máximo de temperatura de la matriz
+double alpha, double height) {
+    //  variable para guardar el cambio máximo de temperatura de la matriz
     double max_change = 0.0;
 
-    // Crear una matriz temporal para almacenar los nuevos valores
+    //  Crear una matriz temporal para almacenar los nuevos valores
     double **temp_matriz = asign_matrix(filas, columnas);
 
-    // Actualizar las temperaturas en la matriz, ignorando los bordes
+    //  Actualizar las temperaturas en la matriz, ignorando los bordes
     for (int i = 1; i < filas - 1; i++) {
         for (int j = 1; j < columnas - 1; j++) {
-            //Fórmula dada
-            double new_temp = matriz[i][j] + (dtime * alpha / (height * height)) * 
-            (matriz[i-1][j] + matriz[i+1][j] + matriz[i][j-1] + matriz[i][j+1] - (4 * matriz[i][j]));
+            //  Fórmula dada
+            double new_temp = matriz[i][j] + (dtime * alpha / (height * height))
+            * (matriz[i-1][j] + matriz[i+1][j] + matriz[i][j-1] +
+            matriz[i][j+1] - (4 * matriz[i][j]));
+
             temp_matriz[i][j] = new_temp;
-            
-            // Calcular el cambio máximo
-            double change = fabs(new_temp - matriz[i][j]);//fabs es para el valor absoluto de un flotante
-            //si hay un mayor cambio hacemos el swap
+            //  Calcular el cambio máximo
+            double change = fabs(new_temp - matriz[i][j]);
+            //  fabs es para el valor absoluto de un flotante
+            //  si hay un mayor cambio hacemos el swap
             if (change > max_change) {
                 max_change = change;
             }
         }
     }
 
-    // Copiar la matriz temporal a la original
+    //  Copiar la matriz temporal a la original
     for (int i = 1; i < filas - 1; i++) {
         for (int j = 1; j < columnas - 1; j++) {
             matriz[i][j] = temp_matriz[i][j];
         }
     }
-    
-    //Liberar matriz
+    //  Liberar matriz
     free_matrix(temp_matriz, filas);
     return max_change;
 }
 
-//implementación para la asignación de la matriz
+//  implementación para la asignación de la matriz
 double** asign_matrix(int filas, int columnas) {
-    //Reservamos espacio para las filas(apuntan a arrays de doubles)
+    //  Reservamos espacio para las filas(apuntan a arrays de doubles)
     double **matriz = (double **)malloc(filas * sizeof(double *));
     for (int i = 0; i < filas; i++) {
-        //Reservamos espacio para las columnas(apuntan a doubles)
+        //  Reservamos espacio para las columnas(apuntan a doubles)
         matriz[i] = (double *)malloc(columnas * sizeof(double));
     }
     return matriz;
 }
 
-//implementación para liberar la matriz de memoria
+//  implementación para liberar la matriz de memoria
 void free_matrix(double **matriz, int filas) {
     for (int i = 0; i < filas; i++) {
         free(matriz[i]);
