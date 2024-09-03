41eed65e7e71669dbc2380e5706e590b3a3c9230
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-03 15:31:13 -0600

    Creación de archivo para manejar todos los archivos y luego escribir el resultado

---
+++ homeworks/serial/src/process_job_file.h
@@ -0,0 +1,8 @@
+//  Copyright [2024] <jose.guerrarodriguez@ucr.ac.cr>
+#ifndef PROCESS_JOB_FILE_H
+#define PROCESS_JOB_FILE_H
+
+
+void process_job_file (const char *job_file, const char *output_dir);
+
+#endif
