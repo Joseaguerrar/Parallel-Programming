566fda040b45551426cf87968cf79d40ea1467cf
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-07 17:25:58 -0600

    Doxygen al producer .h y .c pro cons unbound

--- examples/pthreads/pro_cons_unbound/src/producer.c
+++ examples/pthreads/pro_cons_unbound/src/producer.c
@@ -5,34 +5,58 @@
 #include "common.h"
 #include "producer.h"
 
+/**
+ * @brief Función que ejecuta el hilo productor.
+ * 
+ * Esta función simula el proceso de producción en la simulación. Los productores
+ * generan unidades de trabajo y las colocan en una cola compartida. Si hay unidades
+ * pendientes, el productor toma una unidad y la procesa. Si no, el hilo termina.
+ * 
+ * @param data Puntero a los datos de la simulación (simulation_t).
+ * @return void* Devuelve NULL al finalizar.
+ */
 void* produce(void* data) {
-  // const private_data_t* private_data = (private_data_t*)data;
   simulation_t* simulation = (simulation_t*)data;
 
   while (true) {
     // declare my_unit := 0
     size_t my_unit = 0;
+    // Declara una unidad local para ser producida.
+
     // lock(can_access_next_unit)
     pthread_mutex_lock(&simulation->can_access_next_unit);
+    // Bloquea el acceso a la siguiente unidad para evitar condiciones de carrera.
+
     // If is there pending work, take a unit for producing
     if (simulation->next_unit < simulation->unit_count) {
       my_unit = ++simulation->next_unit;
+      // Si hay unidades pendientes, asigna una al productor.
     } else {
       // unlock(can_access_next_unit)
       pthread_mutex_unlock(&simulation->can_access_next_unit);
+      // Desbloquea el acceso a la siguiente unidad si no hay trabajo pendiente.
+
       // break while
       break;
+      // Termina el bucle si no quedan más unidades por producir.
     }
     // unlock(can_access_next_unit)
     pthread_mutex_unlock(&simulation->can_access_next_unit);
+    // Desbloquea el acceso a la siguiente unidad después de asignarla.
 
-    usleep(1000 * random_between(simulation->producer_min_delay
-      , simulation->producer_max_delay));
+    usleep(1000 * random_between(simulation->producer_min_delay,
+      simulation->producer_max_delay));
+    // Simula el retardo entre la producción de las unidades.
+
     queue_enqueue(&simulation->queue, my_unit);
+    // Encola la unidad producida en la cola compartida.
+
     printf("Produced %zu\n", my_unit);
+    // Imprime el número de la unidad producida.
 
     // signal(can_consume)
     sem_post(&simulation->can_consume);
+    // Señala que hay una nueva unidad disponible para ser consumida.
   }
 
   return NULL;
--- examples/pthreads/pro_cons_unbound/src/producer.h
+++ examples/pthreads/pro_cons_unbound/src/producer.h
@@ -3,6 +3,25 @@
 #ifndef PRODUCER_H
 #define PRODUCER_H
 
+/**
+ * @file producer.h
+ * @brief Declaración de la función `produce` para el hilo productor.
+ * 
+ * Este archivo de cabecera contiene la declaración de la función `produce`,
+ * que implementa la lógica del hilo productor en la simulación.
+ */
+
+/**
+ * @brief Función que ejecuta el hilo productor.
+ * 
+ * Esta función se encarga de generar unidades de trabajo en la simulación. 
+ * Los productores toman unidades pendientes, las procesan y las colocan en 
+ * una cola compartida. La función maneja la sincronización mediante mutexes 
+ * y semáforos para asegurar el acceso correcto a los recursos compartidos.
+ * 
+ * @param data Puntero a los datos de la simulación (generalmente una estructura de simulación).
+ * @return void* Devuelve NULL cuando el hilo productor termina su ejecución.
+ */
 void* produce(void* data);
 
 #endif  // PRODUCER_H
