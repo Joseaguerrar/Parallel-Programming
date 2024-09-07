05e66e8a431d3b53c82381f84f737501606a8144
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-07 17:18:02 -0600

    Doxygen al common .h y .c pro cons unbound

--- examples/pthreads/pro_cons_unbound/src/common.c
+++ examples/pthreads/pro_cons_unbound/src/common.c
@@ -4,6 +4,17 @@
 
 #include "common.h"
 
+/**
+ * @brief Genera un valor aleatorio entre dos límites.
+ * 
+ * Esta función devuelve un valor aleatorio entre los límites mínimo (min) y 
+ * máximo (max) especificados. Si el valor máximo es mayor que el mínimo, 
+ * genera un número en ese rango; de lo contrario, devuelve el valor mínimo.
+ * 
+ * @param min Valor mínimo del rango.
+ * @param max Valor máximo del rango.
+ * @return useconds_t Un valor aleatorio en microsegundos entre min y max.
+ */
 useconds_t random_between(useconds_t min, useconds_t max) {
   return min + (max > min ? (random() % (max - min)) : 0);
 }
