d8bac20a125604499882e83649e3d5dd3fb86009
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-07 17:21:36 -0600

    Doxygen al consumer, main .h y .c pro cons unbound

--- examples/pthreads/pro_cons_unbound/src/consumer.h
+++ examples/pthreads/pro_cons_unbound/src/consumer.h
@@ -3,6 +3,24 @@
 #ifndef CONSUMER_H
 #define CONSUMER_H
 
+/**
+ * @file consumer.h
+ * @brief Declaración de la función `consume` para el hilo consumidor.
+ * 
+ * Este archivo de cabecera contiene la declaración de la función `consume`,
+ * que implementa la lógica del hilo consumidor en la simulación.
+ */
+
+/**
+ * @brief Función que ejecuta el hilo consumidor.
+ * 
+ * Esta función simula el proceso de consumo en la simulación. Los consumidores
+ * toman elementos de una cola compartida, los procesan y esperan un tiempo
+ * aleatorio entre un retardo mínimo y máximo.
+ * 
+ * @param data Puntero a los datos de la simulación (generalmente una estructura de simulación).
+ * @return void* Devuelve NULL al finalizar el consumo.
+ */
 void* consume(void* data);
 
 #endif  // CONSUMER_H
