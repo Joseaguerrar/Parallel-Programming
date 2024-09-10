6515fe0e518e70dbd0ef8da7ee6488af28b00739
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-09 20:44:19 -0600

    Asignar dependencias de las tasks

--- exercises/pthreads/building_taks/src/building_tasks_.c
+++ exercises/pthreads/building_taks/src/building_tasks_.c
@@ -4,13 +4,53 @@
 #include <stdlib.h>
 #include <unistd.h> // Para sleep()
 
-#define job_count 10 //Cantidad de tareas
+#define TASK_COUNT 10 //Cantidad de tareas
 
 // Lista de semáforos, uno para cada tarea
-sem_t job_dependencies[job_count];
+sem_t task_dependencies[TASK_COUNT];
 
-typedef struct{
-    int job_id;
+// Estructura para representar cada tarea
+typedef struct {
+    int task_id;
     int *dependencies;
     int num_dependencies;
-}   jobinfo;
+} TaskInfo;
+
+// Dependencias entre tareas (grafo de tareas)
+int task_deps[TASK_COUNT][2] = {
+    {0, 0},    // 0: Obra gris no depende de nada
+    {0, 1},    // 1: Plomería exterior depende de obra gris
+    {1, 1},    // 2: Pintura exterior depende de plomería exterior
+    {2, 1},    // 3: Acabados exteriores dependen de pintura exterior
+    {1, 1},    // 4: Plomería interior depende de plomería exterior
+    {0, 1},    // 5: Techo depende de obra gris
+    {5, 1},    // 6: Instalación eléctrica depende de techo
+    {4, 2},    // 7: Pintura interior depende de plomería interior e instalación eléctrica
+    {7, 1},    // 8: Piso depende de pintura interior
+    {8, 1}     // 9: Acabados interiores dependen de piso
+};
+
+// Función para simular el trabajo de cada albañil (hilo de trabajo)
+void* builder(void* arg);
+
+int main(int argc, char* argv[]) {
+    pthread_t builders[TASK_COUNT];
+    TaskInfo task_info[TASK_COUNT];
+
+    // Inicializar los semáforos para cada tarea
+    for (int i = 0; i < TASK_COUNT; i++) {
+        sem_init(&task_dependencies[i], 0, 0);
+    }
+
+    // Crear las dependencias para cada tarea
+    for (int i = 0; i < TASK_COUNT; i++) {
+        task_info[i].task_id = i;
+        task_info[i].num_dependencies = task_deps[i][1];
+        task_info[i].dependencies = malloc(task_info[i].num_dependencies * sizeof(int));
+
+        // Asignar dependencias
+        for (int j = 0; j < task_info[i].num_dependencies; j++) {
+            task_info[i].dependencies[j] = task_deps[i][j];
+        }
+    }
+}
