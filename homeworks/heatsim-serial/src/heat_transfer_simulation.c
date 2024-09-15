46306fc09adf43419575bc5b51c3df975897cb53
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-15 00:36:58 -0600

    Asignacion de valores parámetros de la matriz después de leer el archivo y asignar la memoria

--- homeworks/heatsim-serial/src/heat_transfer_simulation.c
+++ homeworks/heatsim-serial/src/heat_transfer_simulation.c
@@ -23,29 +23,43 @@ void read_bin_plate(const char* folder, params_matrix* variables, uint64_t lines
 
         double **matrix =malloc(rows*sizeof(double*));
 
-    if (matrix==NULL)
-    {
-        fprintf(stderr,"Error al asignar memoria para las filas de la matriz\n");
-        fclose(bin_file);
-        return;
-    }
-    
-    for (uint64_t i = 0; i < rows; i++)
-    {
-        matrix[i]=malloc(columns * sizeof(double));
-        if (matrix[i]==NULL)
+        if (matrix==NULL)
         {
-            fprintf(stderr, "Error al asignar memoria para las columnas de la matriz\n");
-            for (uint64_t j = 0; j < i; j++)
-            {
-                free(matrix);
-            }
-            free(matrix);
+            fprintf(stderr,"Error al asignar memoria para las filas de la matriz\n");
             fclose(bin_file);
             return;
-            
         }
         
+        for (uint64_t i = 0; i < rows; i++)
+        {
+            matrix[i]=malloc(columns * sizeof(double));
+            if (matrix[i]==NULL)
+            {
+                fprintf(stderr, "Error al asignar memoria para las columnas de la matriz\n");
+                for (uint64_t j = 0; j < i; j++)
+                {
+                    free(matrix);
+                }
+                free(matrix);
+                fclose(bin_file);
+                return;
+                
+            }
+            
+        }
+        double delta_t = variables[i].delta_t;
+        double alpha = variables[i].alpha;
+        double h = variables[i].h;
+        double epsilon = variables[i].epsilon;
+        uint64_t states_k = simulacion_transferencia_calor(matrix, rows, columns, delta_t, alpha, h, epsilon);
+        array_state_k[i] = states_k;
+        generar_archivo_bin(matrix, rows, columns, folder, variables[i].filename, states_k);
+
+        for (uint64_t i = 0; i < rows; i++) {
+            free(matrix[i]);
+        }
+        free(matrix);
+        fclose(bin_file);
     }
     
 }
