b3fb76320ef170151c449be91d708fd40cd06188
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-15 00:25:27 -0600

    Asignaciones y clicos para verificar errores a la hora de asignar memoria de la matriz con el binario

--- homeworks/heatsim-serial/src/heat_transfer_simulation.c
+++ homeworks/heatsim-serial/src/heat_transfer_simulation.c
@@ -10,5 +10,26 @@ void read_bin_plate(const char* folder, params_matrix* variables, uint64_t lines
     char direction[512];
     uint64_t* array_state_k=malloc(lines * sizeof(uint64_t));
 
+    for (uint64_t i = 0; i < lines; i++) {
+        snprintf(direction, sizeof(direction), "%s/%s", folder, variables[i].filename);
+        bin_file = fopen(direction, "rb");
+        if (bin_file == NULL) {
+            fprintf(stderr, "No se pudo abrir el archivo binario %s\n", variables[i].filename);
+            return;
+        }
+
+        fread(&rows, sizeof(uint64_t), 1, bin_file);
+        fread(&columns, sizeof(uint64_t), 1, bin_file);
+
+        double **matrix =malloc(rows*sizeof(double*));
+
+    if (matrix==NULL)
+    {
+        fprintf(stderr,"Error al asignar memoria para la matriz\n");
+        fclose(bin_file);
+        return;
+    }
+    
+
 }
 
