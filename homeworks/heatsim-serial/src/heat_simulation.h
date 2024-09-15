ae85ce6be667d02f65c12d327e61fbc6e00ce581
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-14 23:30:52 -0600

    Primera parte de la implementación para leer archivo binario

--- homeworks/heatsim-serial/src/heat_simulation.h
+++ homeworks/heatsim-serial/src/heat_simulation.h
@@ -14,19 +14,19 @@ typedef struct {
     double alpha;         // Difusividad térmica
     double h;             // Tamaño de las celdas
     double epsilon;       // Sensitividad del punto de equilibrio
-} formula_p;
+} params_matrix;
 
 // Funciones para leer los archivos de simulación
-formula_p* read_job_txt(const char* jobName, const char* folder, uint64_t* lines);
+params_matrix* read_job_txt(const char* jobName, const char* folder, uint64_t* lines);
 uint64_t count_lines(const char* fileName);
 
 // Funciones para la simulación de transferencia de calor
-void read_plate_bin(const char* folder, formula_p* variables_formula, uint64_t lines, const char* jobName);
+void read_bin_plate(const char* folder, params_matrix* variables_formula, uint64_t lines, const char* jobName);
 uint64_t heat_transfer_simulation(double** matrix, uint64_t rows, uint64_t columns, double delta_t, double alpha, double h, double epsilon);
 char* format_time(const time_t seconds, char* text, const size_t capacity);
 
 // Funciones para generar archivos de salida
-void generate_report_file(const char* folder, const char* jobName, formula_p* variables_formula, uint64_t* state_k, uint64_t lines);
+void generate_report_file(const char* folder, const char* jobName, params_matrix* variables_formula, uint64_t* state_k, uint64_t lines);
 void generate_bin_file(double** matrix, uint64_t rows, uint64_t columns, const char* folder, const char* jobName, uint64_t state_k);
 
 #endif // HEAT_SIMULATION_H
