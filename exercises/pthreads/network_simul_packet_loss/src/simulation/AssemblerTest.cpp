69caa2d54af775d1ace52f036faa67c291b38fcc
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-18 16:33:49 -0600

    Implementación de la función consume del assembler

---
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.cpp
@@ -0,0 +1,17 @@
+#include "AssemblerTest.hpp"
+#include "Util.hpp"
+void AssemblerTest::consume(NetworkMessage data){
+    (void)data;
+    //Le sumamos la cantidad de mensajes recibidos
+    this->receivedMessages++;
+    //Un double random en 0 y 100
+    double random = 1 + Util::random(0,100);
+    if (random> this->loss_probability)
+    {
+        //Lo mandamos a dormir
+        Util::sleepFor(this->producer_delay);
+        produce(createMessageRand());
+        std::cout<< "Mensaje perdido/Message lost"<< std:: endl;
+    }
+    
+}
