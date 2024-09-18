a01f7c20b4b1e7b4f043d9e040c75769c7eec6c1
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-18 17:04:39 -0600

    Implementación del create rand message

--- exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.cpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.cpp
@@ -10,7 +10,7 @@ void AssemblerTest::consume(NetworkMessage data){
     {
         //Lo mandamos a dormir
         Util::sleepFor(this->producer_delay);
-        produce(createMessageRand());
+        produce(create_rand_message());
         std::cout<< "Mensaje perdido/Message lost"<< std:: endl;
     }   
 }
@@ -22,4 +22,20 @@ int AssemblerTest::run(){
 
 Queue<NetworkMessage>* AssemblerTest:: getQueue(){
     return this->consumingQueue;
+}
+
+NetworkMessage AssemblerTest::create_rand_message() const{
+    //Siempre es 1: este productor
+    const uint16_t source = this->id;
+
+    int temp_target = 1;
+
+    while (temp_target==id)
+    {
+        temp_target = Util::random(1,this->consumer_count);
+    }
+
+    const uint16_t target = temp_target;
+    return NetworkMessage(target,source);
+    
 }
