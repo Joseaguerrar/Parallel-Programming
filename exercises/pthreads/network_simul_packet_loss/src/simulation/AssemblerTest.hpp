f4ea860c0c054258d5ae6c6a050f74012262985c
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-18 16:02:02 -0600

    Constructor del assembler

--- exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.hpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.hpp
@@ -9,15 +9,22 @@ class AssemblerTest
   :public Assembler<NetworkMessage, NetworkMessage> {
   DISABLE_COPY(AssemblerTest);
   protected: 
-  // Numeros de mensajes recibidos
-  int lossProbability = 0;
-  int consumerCount = 0;
-  // Delay del consumidor para consumir un paquete
-  int producerDelay = 0;
-  // Numbero de mensajes recibidos
-  size_t receivedMessages = 0;
-  int id = 0;
-  
+    // Numeros de mensajes recibidos
+    int loss_probability = 0;
+    int consumer_count = 0;
+    // Delay del consumidor para consumir un paquete
+    int producer_delay = 0;
+    // Numbero de mensajes recibidos
+    size_t receivedMessages = 0;
+    int id = 0;
+  public:
+    //Constructor
+    explicit AssemblerTest(int loss_probability, int consumer_count, int producer_delay, int id):Assembler() {
+    this->loss_probability = loss_probability;
+    this->consumer_count = consumer_count;
+    this->producer_delay = producer_delay;
+    this->id = id;
+  }
 };
 
 #endif
