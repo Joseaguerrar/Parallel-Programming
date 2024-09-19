3b0ce2463e335e769bf8919852d06294d93c4ef8
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-18 23:00:35 -0600

    Clase assembler con sus protected

--- exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.hpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.hpp
@@ -1,5 +1,20 @@
 #ifndef ASSEMBLER_TEST_HPP
 #define ASSEMBLER_TEST_HPP
 
+#include "Assembler.hpp"
+#include "NetworkMessage.hpp"
+ /**
+ * @brief A AssemblerTest class example
+ */
+class AssemblerTest : public Assembler<NetworkMessage, NetworkMessage> {
+  DISABLE_COPY(AssemblerTest);
 
+ protected:
+  /// Percent of package loss
+  double packageLossPercent = 0.0;
+  /// Amount of consumer on the network
+  size_t consumerCount = 0;
+  /// Amount of message lost
+  size_t packageLossCount = 0;
+};
 #endif
