e5cbed053fc6ea766ac137ad73de81377d5442da
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-18 23:05:19 -0600

    Métodos públicos del assembler

--- exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.hpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.hpp
@@ -11,10 +11,19 @@ class AssemblerTest : public Assembler<NetworkMessage, NetworkMessage> {
 
  protected:
   /// Percent of package loss
-  double packageLossPercent = 0.0;
+  double package_loss_percent = 0.0;
   /// Amount of consumer on the network
-  size_t consumerCount = 0;
+  size_t consumer_count = 0;
   /// Amount of message lost
-  size_t packageLossCount = 0;
+  size_t package_loss_count = 0;
+ public:
+ ///Constructor
+  explicit AssemblerTest(const double package_loss_percent,
+                        const size_t consumer_count);
+ ///Consumir los mensajes en su propio hilo
+ int run() override;
+
+ ///Override para cualquier tipo de dato
+ void consume(NetworkMessage data) override;
 };
 #endif
