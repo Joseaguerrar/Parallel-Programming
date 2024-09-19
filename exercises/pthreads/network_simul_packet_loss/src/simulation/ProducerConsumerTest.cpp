d6b583aac0067f490157ef43dd725743ebbc5c05
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-19 17:44:16 -0600

    Doxygen al hpp y cpp de producer consumer test

--- exercises/pthreads/network_simul_packet_loss/src/simulation/ProducerConsumerTest.cpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/ProducerConsumerTest.cpp
@@ -1,5 +1,5 @@
-/// @copyright 2020 ECCI, Universidad de Costa Rica. All rights reserved
-/// This code is released under the GNU Public License version 3
+/// @copyright 2020 ECCI, Universidad de Costa Rica.
+/// All rights reserved. This code is released under the GNU Public License version 3.
 /// @author Jeisson Hidalgo-Céspedes <jeisson.hidalgo@ucr.ac.cr>
 
 #include <cstdlib>
@@ -11,6 +11,7 @@
 #include "ProducerTest.hpp"
 #include "AssemblerTest.hpp"
 
+/// Usage message for the program, detailing expected arguments.
 const char* const usage =
   "Usage: prodcons packages consumers prod_delay disp_delay cons_delay\n"
   "\n"
@@ -19,10 +20,16 @@ const char* const usage =
   "  prod_delay  delay of producer to create a package\n"
   "  disp_delay  delay of dispatcher to dispatch a package\n"
   "  cons_delay  delay of consumer to consume a package\n"
-  "  loss_percent percentage of lossing a package (0 to 100)\n"
+  "  loss_percent percentage of losing a package (0 to 100)\n"
   "\n"
-  "Delays are in millisenconds, negatives are maximums for random delays\n";
+  "Delays are in milliseconds, negatives are maximums for random delays\n";
 
+/**
+ * @brief Destructor of ProducerConsumerTest.
+ * 
+ * Cleans up and deletes all dynamically allocated objects such as producer,
+ * dispatcher, consumers, and assembler instances.
+ */
 ProducerConsumerTest::~ProducerConsumerTest() {
   delete this->producer;
   delete this->dispatcher;
@@ -31,6 +38,16 @@ ProducerConsumerTest::~ProducerConsumerTest() {
   delete this->assembler;
 }
 
+/**
+ * @brief Starts the producer-consumer simulation by parsing arguments and creating simulation objects.
+ * 
+ * This method creates and initializes the necessary objects like Producer, Dispatcher, Consumers, 
+ * and Assembler, sets up their interactions, starts their threads, and waits for them to finish.
+ * 
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return int Returns EXIT_SUCCESS if the simulation runs successfully, or EXIT_FAILURE if there is an error.
+ */
 int ProducerConsumerTest::start(int argc, char* argv[]) {
   // Parse arguments and store values in object members
   if ( int error = this->analyzeArguments(argc, argv) ) {
@@ -38,43 +55,48 @@ int ProducerConsumerTest::start(int argc, char* argv[]) {
   }
 
   // Create objects for the simulation
-  this->producer = new ProducerTest(this->packageCount, this->productorDelay
-    , this->consumerCount+1);
+  this->producer = new ProducerTest(this->packageCount, this->productorDelay,
+                                    this->consumerCount + 1);
   this->dispatcher = new DispatcherTest(this->dispatcherDelay);
   this->dispatcher->createOwnQueue();
-  // Create each producer
+
+  // Create each consumer
   this->consumers.resize(this->consumerCount);
   for ( size_t index = 0; index < this->consumerCount; ++index ) {
     this->consumers[index] = new ConsumerTest(this->consumerDelay);
     assert(this->consumers[index]);
     this->consumers[index]->createOwnQueue();
   }
-  //Creamos el assembler
-  this->assembler=new AssemblerTest(this->package_loss_percent,
-                                    this->consumerCount);
-  //Creamos su queue
+
+  // Create the assembler
+  this->assembler = new AssemblerTest(this->package_loss_percent, this->consumerCount);
   this->assembler->createOwnQueue();
+
   // Communicate simulation objects
-  // Producer push network messages to the dispatcher queue
+  // Producer pushes network messages to the dispatcher queue
   this->producer->setProducingQueue(this->dispatcher->getConsumingQueue());
+  
   // Dispatcher delivers to each consumer, and they should be registered
   for ( size_t index = 0; index < this->consumerCount; ++index ) {
-    this->dispatcher->registerRedirect(index + 1
-      , this->consumers[index]->getConsumingQueue());
+    this->dispatcher->registerRedirect(index + 1, this->consumers[index]->getConsumingQueue());
   }
-  //Redirigimos la cola del assembler con la del dispatcher
-  this->dispatcher->registerRedirect(this->consumerCount+1,
-                                     this->assembler->getConsumingQueue());
+
+  // Redirect assembler's queue with the dispatcher
+  this->dispatcher->registerRedirect(this->consumerCount + 1,
+                                      this->assembler->getConsumingQueue());
   this->assembler->setProducingQueue(this->dispatcher->getConsumingQueue());
-  // Start the simulation
+
+  // Start the simulation threads
   this->producer->startThread();
   this->dispatcher->startThread();
   for ( size_t index = 0; index < this->consumerCount; ++index ) {
     this->consumers[index]->startThread();
   }
-  //Ejecutamos el assembler
+
+  // Start the assembler thread
   this->assembler->startThread();
-  // Wait for objets to finish the simulation
+
+  // Wait for objects to finish the simulation
   this->producer->waitToFinish();
   this->dispatcher->waitToFinish();
   for ( size_t index = 0; index < this->consumerCount; ++index ) {
@@ -86,10 +108,20 @@ int ProducerConsumerTest::start(int argc, char* argv[]) {
   return EXIT_SUCCESS;
 }
 
+/**
+ * @brief Parses and validates the command-line arguments.
+ * 
+ * This method extracts the values for package count, consumer count, producer delay, 
+ * dispatcher delay, consumer delay, and packet loss percentage from the command-line arguments.
+ * 
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return int Returns EXIT_SUCCESS if arguments are valid, or EXIT_FAILURE if there are errors.
+ */
 int ProducerConsumerTest::analyzeArguments(int argc, char* argv[]) {
   // 5 + 2 arguments are mandatory
   if ( argc != 7 ) {
-    //Ahora son 7 argumentos
+    // Updated to expect 7 arguments
     std::cout << usage;
     return EXIT_FAILURE;
   }
@@ -100,9 +132,9 @@ int ProducerConsumerTest::analyzeArguments(int argc, char* argv[]) {
   this->productorDelay = std::atoi(argv[index++]);
   this->dispatcherDelay = std::atoi(argv[index++]);
   this->consumerDelay = std::atoi(argv[index++]);
-  this->package_loss_percent = std::atof(argv[index++]);
-  //atof es de double
+  this->package_loss_percent = std::atof(argv[index++]); 
+                              // Convert string to double
 
-  // todo: Validate that given arguments are fine
+  // TODO: Validate that given arguments are valid
   return EXIT_SUCCESS;
 }
--- exercises/pthreads/network_simul_packet_loss/src/simulation/ProducerConsumerTest.hpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/ProducerConsumerTest.hpp
@@ -1,5 +1,5 @@
-/// @copyright 2020 ECCI, Universidad de Costa Rica. All rights reserved
-/// This code is released under the GNU Public License version 3
+/// @copyright 2020 ECCI, Universidad de Costa Rica.
+/// All rights reserved. This code is released under the GNU Public License version 3.
 /// @author Jeisson Hidalgo-Céspedes <jeisson.hidalgo@ucr.ac.cr>
 
 #ifndef PRODUCERCONSUMERTEST_HPP
@@ -18,45 +18,83 @@ class ProducerTest;
 
 /**
  * @brief Controller class that simulates the message passing between
- * producer and cosumers through a dispatcher
+ * producer and consumers through a dispatcher.
+ * 
+ * This class orchestrates the creation of the producer, dispatcher, consumers, 
+ * and assembler objects. It sets up communication between them, starts the 
+ * threads for each component, and handles the simulation of packet loss.
  */
 class ProducerConsumerTest {
-  DISABLE_COPY(ProducerConsumerTest);
+  DISABLE_COPY(ProducerConsumerTest);  
+  ///< Macro to disable copy constructor and assignment.
 
  private:
-  /// Number of packages to be produced
+  /// Number of packages to be produced.
   size_t packageCount = 0;
-  /// Number of consumer threads
+  
+  /// Number of consumer threads.
   size_t consumerCount = 0;
-  /// Delay of producer to create a package, negative for max random
+  
+  /// Delay of producer to create a package, negative for max random delay.
   int productorDelay = 0;
-  /// Delay of dispatcher to dispatch a package, negative for max random
+  
+  /// Delay of dispatcher to dispatch a package, negative for max random delay.
   int dispatcherDelay = 0;
-  /// Delay of consumer to consume a package, negative for max random
+  
+  /// Delay of consumer to consume a package, negative for max random delay.
   int consumerDelay = 0;
-  /// Porcentaje de pérdida
-  double package_loss_percent=0.0;
+  
+  /// Percentage of packet loss during transmission.
+  double package_loss_percent = 0.0;
 
  private:
-  /// Producer of the simulated network messages
+  /// Producer of the simulated network messages.
   ProducerTest* producer = nullptr;
-  /// A dispatcher of the of the simulated network messages
+  
+  /// Dispatcher that sends the network messages to consumers.
   DispatcherTest* dispatcher = nullptr;
-  /// Consumers of the simulated network messages
+  
+  /// Consumers that receive and process the network messages.
   std::vector<ConsumerTest*> consumers;
-  /// Simulación de pérdida de paquetes
+  
+  /// Assembler to simulate packet loss in the network.
   AssemblerTest* assembler = nullptr;
 
  public:
-  /// Constructor
+  /**
+   * @brief Default constructor.
+   */
   ProducerConsumerTest() = default;
-  /// Destructor
+
+  /**
+   * @brief Destructor. Cleans up the dynamically allocated resources.
+   */
   ~ProducerConsumerTest();
-  /// Start the simulation
+
+  /**
+   * @brief Starts the producer-consumer simulation.
+   * 
+   * This method initializes the simulation components, starts their threads,
+   * and manages their interaction throughout the simulation.
+   * 
+   * @param argc Number of command-line arguments.
+   * @param argv Array of command-line arguments.
+   * @return int Returns EXIT_SUCCESS if simulation runs successfully, otherwise EXIT_FAILURE.
+   */
   int start(int argc, char* argv[]);
 
  private:
-  /// Analyze the command line arguments
+  /**
+   * @brief Analyzes the command-line arguments provided to the program.
+   * 
+   * This method validates and stores the values provided for the number of packages,
+   * the number of consumers, and the delays for each component, as well as the 
+   * packet loss percentage.
+   * 
+   * @param argc Number of command-line arguments.
+   * @param argv Array of command-line arguments.
+   * @return int Returns EXIT_SUCCESS if arguments are valid, or EXIT_FAILURE if there's an error.
+   */
   int analyzeArguments(int argc, char* argv[]);
 };
 
