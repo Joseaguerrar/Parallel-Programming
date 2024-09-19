f2f55dad966083f2e1c26a89ebea3ad8afcd2b29
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-19 17:49:43 -0600

    Doxygen al hpp y cpp de producer test

--- exercises/pthreads/network_simul_packet_loss/src/simulation/ProducerTest.cpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/ProducerTest.cpp
@@ -1,42 +1,71 @@
-/// @copyright 2020 ECCI, Universidad de Costa Rica. All rights reserved
-/// This code is released under the GNU Public License version 3
+/// @copyright 2020 ECCI, Universidad de Costa Rica.
+/// All rights reserved. This code is released under the GNU Public License version 3.
 /// @author Jeisson Hidalgo-Céspedes <jeisson.hidalgo@ucr.ac.cr>
 
 #include "ProducerTest.hpp"
 #include "Log.hpp"
 #include "Util.hpp"
 
-ProducerTest::ProducerTest(size_t packageCount, int productorDelay
-  , size_t consumerCount)
+/**
+ * @brief Constructs a ProducerTest object.
+ * 
+ * This constructor initializes the producer with the number of packages to create, 
+ * the delay for producing each message, and the number of consumers to send messages to.
+ * 
+ * @param packageCount The number of messages (packages) to produce.
+ * @param productorDelay The delay for each message creation (in milliseconds).
+ * @param consumerCount The number of consumers the producer can send messages to.
+ */
+ProducerTest::ProducerTest(size_t packageCount, int productorDelay, size_t consumerCount)
   : packageCount(packageCount)
   , productorDelay(productorDelay)
   , consumerCount(consumerCount) {
 }
 
+/**
+ * @brief Starts the production process of network messages.
+ * 
+ * This method loops through the number of packages specified during construction, 
+ * creates and sends each message, and signals the end of production by sending an 
+ * empty message.
+ * 
+ * @return int Returns EXIT_SUCCESS upon successful completion of production.
+ */
 int ProducerTest::run() {
-  // Produce each asked message
+  // Produce each requested message
   for ( size_t index = 0; index < this->packageCount; ++index ) {
     this->produce(this->createMessage(index));
   }
 
-  // Produce an empty message to communicate we finished
+  // Produce an empty message to signal completion
   this->produce(NetworkMessage());
 
-  // Report production is done
+  // Log that production has completed
   Log::append(Log::INFO, "Producer", std::to_string(this->packageCount)
     + " messages sent");
   return EXIT_SUCCESS;
 }
 
+/**
+ * @brief Creates a network message.
+ * 
+ * This method generates a network message with the current index. The source of the message
+ * is always the producer (source = 1), and the target is randomly selected from the available consumers.
+ * The delay for producing each message is simulated with a sleep.
+ * 
+ * @param index The current index of the message being produced.
+ * @return NetworkMessage A message containing the source, target, and index.
+ */
 NetworkMessage ProducerTest::createMessage(size_t index) const {
   // Source is always 1: this producer
   const uint16_t source = 1;
-  // Target is selected by random
-  const uint16_t target = 1 + Util::random(0
-    , static_cast<int>(this->consumerCount));
-  // IMPORTANT: This simulation uses sleep() to mimics the process of
-  // producing a message. However, you must NEVER use sleep() for real projects
+  
+  // Target is selected randomly from the consumers
+  const uint16_t target = 1 + Util::random(0, static_cast<int>(this->consumerCount));
+  
+  // Simulate the delay for producing the message (using sleep)
   Util::sleepFor(this->productorDelay);
+  
   // Create and return a copy of the network message
   return NetworkMessage(target, source, index);
 }
--- exercises/pthreads/network_simul_packet_loss/src/simulation/ProducerTest.hpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/ProducerTest.hpp
@@ -1,5 +1,5 @@
-/// @copyright 2020 ECCI, Universidad de Costa Rica. All rights reserved
-/// This code is released under the GNU Public License version 3
+/// @copyright 2020 ECCI, Universidad de Costa Rica.
+/// All rights reserved. This code is released under the GNU Public License version 3.
 /// @author Jeisson Hidalgo-Céspedes <jeisson.hidalgo@ucr.ac.cr>
 
 #ifndef PRODUCERTEST_HPP
@@ -9,26 +9,54 @@
 #include "Producer.hpp"
 
 /**
- * @brief A productor class example
- * Produces network messages and push them to the queue
+ * @brief Example class of a producer.
+ * 
+ * This class is responsible for producing network messages and pushing them 
+ * to a queue. It simulates the process of message creation with an optional delay
+ * and assigns each message a target consumer.
  */
 class ProducerTest : public Producer<NetworkMessage> {
-  DISABLE_COPY(ProducerTest);
+  DISABLE_COPY(ProducerTest);  ///< Macro to disable copy constructor and assignment.
 
  protected:
-  /// Number of packages to be produced
+  /// Number of packages to be produced.
   size_t packageCount = 0;
-  /// Delay of producer to create a package, negative for max random
+
+  /// Delay of producer to create a package, negative for max random delay.
   int productorDelay = 0;
-  /// Number of consumer threads
+
+  /// Number of consumer threads available to receive messages.
   size_t consumerCount = 0;
 
  public:
-  /// Constructor
+  /**
+   * @brief Constructor to initialize the producer with simulation parameters.
+   * 
+   * @param packageCount The number of packages to be produced.
+   * @param productorDelay The delay between producing packages (in milliseconds). A negative value indicates a maximum random delay.
+   * @param consumerCount The number of consumers that will receive the packages.
+   */
   ProducerTest(size_t packageCount, int productorDelay, size_t consumerCount);
-  /// Do the message production in its own execution thread
+
+  /**
+   * @brief Executes the message production process in its own execution thread.
+   * 
+   * This method is responsible for creating and sending the specified number of messages
+   * and signaling the end of production by sending an empty message.
+   * 
+   * @return int Returns EXIT_SUCCESS upon successful completion of message production.
+   */
   int run() override;
-  /// Creates a simulation message to be sent by the network
+
+  /**
+   * @brief Creates a network message to be sent.
+   * 
+   * This method generates a message with the current index, assigning it a random target
+   * consumer and simulating a delay in production.
+   * 
+   * @param index The index of the message being created.
+   * @return NetworkMessage A message containing the source, target, and index.
+   */
   NetworkMessage createMessage(size_t index) const;
 };
 
