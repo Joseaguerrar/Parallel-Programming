c32f3818f1042249c46fc977f6d271babab93991
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-18 23:11:23 -0600

    Implementación del constructor y función run

--- exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.cpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.cpp
@@ -1,2 +1,20 @@
 #include "AssemblerTest.hpp"
 #include "Util.hpp"
+#include "Log.hpp"
+
+AssemblerTest::AssemblerTest(const double package_loss_percent,
+                             const size_t consumer_count)
+                             : package_loss_percent(package_loss_percent),
+                              consumer_count(consumer_count){
+}
+
+
+int AssemblerTest::run(){
+    // Empezar el loop para consumir los mensajes
+    this->consumeForever();
+
+    //Cuando termine, imprimir los datos
+    Log::append(Log::INFO, "Assembler", std::to_string(this->package_loss_count)
+                + "messages lost");
+    return EXIT_SUCCESS;
+}
