0b75dd3ee6d1b87b48f76e5dea35945ca67c833c
joseaguerrar <joseaguerrar2017@gmail.com>
2024-10-16 17:56:16 -0600

    Omp_team con lint, doxy y todo

--- exercises/openmp/omp_team/src/main.cpp
+++ exercises/openmp/omp_team/src/main.cpp
@@ -1,12 +1,23 @@
 // Copyright 2024 ECCI-UCR CC-BY 4.0
 #include <iostream>
-#include <omp.h>
+#include <omp.h> //NOLINT
 #include <cstdlib>
 
+/**
+ * @brief Función principal del programa.
+ * 
+ * Esta función determina el número de hilos a utilizar, establece la cantidad de hilos y luego imprime 
+ * un mensaje desde cada hilo en una sección crítica para evitar la mezcla de la salida.
+ * 
+ * @param argc Número de argumentos pasados por línea de comandos.
+ * @param argv Array de punteros a los argumentos pasados por línea de comandos.
+ * @return int Código de salida del programa.
+ */
 int main(int argc, char* argv[]) {
     // Determinar el número de hilos
-    const int thread_count = (argc == 2) ? std::atoi(argv[1]) : omp_get_num_procs();
-    
+    const int thread_count = (argc == 2) ? std::atoi(argv[1]) :
+                                                            omp_get_num_procs();
+
     // Establecer el número de hilos
     omp_set_num_threads(thread_count);
 
@@ -19,7 +30,7 @@ int main(int argc, char* argv[]) {
         // Usamos una sección crítica para evitar la mezcla en la salida
         #pragma omp critical
         {
-            std::cout << "Hello from secondary thread " << thread_id 
+            std::cout << "Hello from secondary thread " << thread_id
                       << " of " << thread_count << std::endl;
         }
     }
