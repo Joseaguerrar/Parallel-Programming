80fbba386faa5e1bf18eb1b578b7a741093e9735
joseaguerrar <joseaguerrar2017@gmail.com>
2024-10-16 18:14:11 -0600

    Doxy, lint y cambio al makefile para hello world omp

--- examples/openmp/hello_world/hello.cpp
+++ examples/openmp/hello_world/hello.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 Jeisson Hidalgo <jeisson.hidalgo@ucr.ac.cr> CC-BY 4.0
 #include <iostream>
 
+/**
+ * @brief Función principal que ejecuta el programa.
+ * 
+ * Esta función crea múltiples hilos usando OpenMP y cada hilo imprime un mensaje. 
+ * La sección crítica de OpenMP garantiza que la salida de cada hilo no se mezcle.
+ * 
+ * @return int Código de salida del programa.
+ */
 int main() {
   #pragma omp parallel
   {
