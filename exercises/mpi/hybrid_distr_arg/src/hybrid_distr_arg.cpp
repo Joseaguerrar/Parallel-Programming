5aab21644d6bbedc9f0fd3a003c58680c9c8d2c6
joseaguerrar <joseaguerrar2017@gmail.com>
2024-11-05 19:51:51 -0600

    Ejercicio listo

--- exercises/mpi/hybrid_distr_arg/src/hybrid_distr_arg.cpp
+++ exercises/mpi/hybrid_distr_arg/src/hybrid_distr_arg.cpp
@@ -23,23 +23,29 @@ int main(int argc, char* argv[]) {
       const int overall_start = atoi(argv[1]);
       const int overall_finish = atoi(argv[2]);
 
-      const int process_start = calculate_start(process_number, overall_finish
-        , process_count, overall_start);
-      const int process_finish = calculate_finish(process_number, overall_finish
-        , process_count, overall_start);
+      const int process_start = calculate_start(process_number, overall_finish, process_count, overall_start);
+      const int process_finish = calculate_finish(process_number, overall_finish, process_count, overall_start);
       const int process_size = process_finish - process_start;
 
       std::cout << process_hostname << ':' << process_number << ": range [" 
-        << process_start << ", " << process_finish << "[ size " << process_size
-        << std::endl;
+                << process_start << ", " << process_finish << "[ size " << process_size
+                << std::endl;
 
-      int thread_start = -1;
-      int thread_finish = -1;
-      const int thread_size = thread_finish - thread_start;
+      #pragma omp parallel
+      {
+        int thread_id = omp_get_thread_num();
+        int thread_count = omp_get_num_threads();
 
-      std::cout << '\t' << process_hostname << ':' << process_number << '.'
-        << omp_get_thread_num() << ": range [" << thread_start << ", " <<
-        thread_finish << "[ size " << thread_size << std::endl;
+        // Calcular el rango para cada hilo
+        int thread_start = calculate_start(thread_id, process_finish, thread_count, process_start);
+        int thread_finish = calculate_finish(thread_id, process_finish, thread_count, process_start);
+        int thread_size = thread_finish - thread_start;
+
+        #pragma omp critical
+        std::cout << '\t' << process_hostname << ':' << process_number << '.'
+                  << thread_id << ": range [" << thread_start << ", " << thread_finish 
+                  << "[ size " << thread_size << std::endl;
+      }
     } else {
       std::cerr << "usage: hybrid_distr_arg start finish" << std::endl;
     }
@@ -51,7 +57,7 @@ int main(int argc, char* argv[]) {
 }
 
 int calculate_start(int rank, int end, int workers, int begin) {
-  // ...
+  return rank * ((end - begin) / workers) + std::min(rank, (end - begin) % workers);
 }
 
 int calculate_finish(int rank, int end, int workers, int begin) {
